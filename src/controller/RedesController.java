4c21d44693bee2d92ec5c7721260962ed0719f83
André <andre0208oliveira@gmail.com>
2025-03-01 18:46:58 -0300

    primeiro commit

---
+++ src/controller/RedesController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class RedesController {
+
+	public RedesController() {
+		super();
+	}
+	
+	private String so() {
+		String osName = System.getProperty("os.name");
+		return (osName);
+	}
+}
