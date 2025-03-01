33e576829e511859bbf0b38be41267ab2a5e03af
André <andre0208oliveira@gmail.com>
2025-03-01 20:35:39 -0300

    atualização

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RedesController {
 
 	public RedesController() {
@@ -11,8 +14,36 @@ public class RedesController {
 		return osName;
 	}
 	
+	@SuppressWarnings("deprecation")
 	public void ip() {
-		if(os().contains("Windows"));
-		
-	}
+		String proc = "";
+		if(os().contains("Windows")) {
+			proc = "IPCONFIG";
+			
+		}else if(os().contains("Linux")) {
+			proc = "ifconfig";
+		}
+			try {
+				Process p = Runtime.getRuntime().exec(proc);
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String linha = buffer.readLine();
+				while( linha != null) {
+					if(linha.contains("Adaptador")) {
+						while(linha != "Adaptador") {
+							if(linha.contains("Endereço IPV4")) {
+								System.out.println(linha);
+							}
+							linha = buffer.readLine();
+						}
+						System.out.println(linha);
+					}
+					linha = buffer.readLine();	
+				}
+				}catch(Exception e){
+				System.err.println(e.getMessage());
+			}
+	}	
+	
+	
 }
+
